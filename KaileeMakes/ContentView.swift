8cc1803435fac578ab49bcec6f87f202ca08fc7f
kaileehigginsott <ksho2003@gmail.com>
2023-05-10 12:02:06 -0600

    Initial Commit

---
+++ KaileeMakes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KaileeMakes
+//
+//  Created by Kailee Higgins Ott on 5/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KaileeMakes/KaileeMakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaileeMakesApp.swift
+//  KaileeMakes
+//
+//  Created by Kailee Higgins Ott on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KaileeMakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
